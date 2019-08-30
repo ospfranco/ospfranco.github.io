9f34c6d78690ddb9716f0c120b1294beb44c5bbe
Oscar Franco <ospfranco@protonmail.com>
2019-08-30 20:56:23 +0200

    Fixed deploymnet

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ if [ -d _site ]; then
   rm -rdf _site 
 fi
 
-bundle exec jekyll build -b https://ospfranco.github.io 
+JEKYLL_ENV=production bundle exec jekyll build -b https://ospfranco.github.io 
 
 cd _site 
 git init
