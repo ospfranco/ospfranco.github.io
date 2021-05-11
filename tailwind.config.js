68b1531aae2eb1dc03dbef5f8e059c153d6c3ad1
Oscar Franco <ospfranco@protonmail.com>
2021-05-11 13:43:07 +0200

    New design

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        xl: '2rem'
+      }
+    },
   },
   variants: {},
   plugins: [],
