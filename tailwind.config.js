e25ca016b98f00fc96113f3cbc8ff62bcf348763
Oscar Franco <ospfranco@protonmail.com>
2021-08-02 22:22:59 +0200

    more minimalism

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,23 @@
 module.exports = {
-  purge: [
-    './src/**.html'
-  ],
-  darkMode: 'media',
+  purge: ["./src/**.html"],
+  darkMode: "media",
   theme: {
     colors: {
-      ...require('tailwindcss/colors')
+      ...require("tailwindcss/colors"),
+      bgGray: {
+        DEFAULT: "#0b0b0c",
+      },
     },
     extend: {
       borderRadius: {
-        xl: '2rem'
+        xl: "2rem",
       },
       width: {
-        600: '600px',
-        768: '768px',
+        600: "600px",
+        768: "768px",
       },
     },
   },
   variants: {},
-  plugins: [
-    require('tailwind-scrollbar-hide')
-  ],
-}
+  plugins: [require("tailwind-scrollbar-hide")],
+};
